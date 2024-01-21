efad2c3a4c666fb096c86a4b09007d25e1f18b94
TotallyFarhan <66565336+TotallyFarhan@users.noreply.github.com>
2024-01-21 14:30:36 -0500

    added new paths

--- src/main/java/frc/robot/commands/Pickup.java
+++ src/main/java/frc/robot/commands/Pickup.java
@@ -4,19 +4,13 @@
 
 package frc.robot.commands;
 
-<<<<<<< HEAD
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Intake;
+
 
 public class Pickup extends Command {
   /** Creates a new Pickup. */
   public Intake intake = new Intake();
-=======
-import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Pickup extends CommandBase {
-  /** Creates a new Pickup. */
->>>>>>> 955c611fade44587762345266774982758d873b0
   public Pickup() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -27,7 +21,6 @@ public class Pickup extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-<<<<<<< HEAD
   public void execute() {
     intake.open();
     intake.setSpeed(.5);    ///TODO fix the speed and direction 
@@ -38,21 +31,10 @@ public class Pickup extends CommandBase {
   public void end(boolean interrupted) {
     intake.stop();
   }
-=======
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
->>>>>>> 955c611fade44587762345266774982758d873b0
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-<<<<<<< HEAD
     return intake.loaded();
-=======
-    return false;
->>>>>>> 955c611fade44587762345266774982758d873b0
   }
 }
